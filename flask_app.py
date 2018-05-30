c5b8664b50b8c926f1abfd5c2ddfad5c9124bf71
Makasz <MakaszML@gmail.com>
2018-05-30 15:51:15 +0200

    Printing 10 first patients in plain text.

--- flask_app.py
+++ flask_app.py
@@ -72,21 +72,21 @@ def _get_med_name(prescription, client=None):
 @app.route('/index.html')
 def index():
     smart = _get_smart()
-    body = "<h1>Hello</h1>"
+    body = "<h1>Portal medyczny Jerzego Zięby</h1>"
     patient = pat.Patient.read('06eb35fc-09e6-48b4-a311-47633f6c4769', smart.server)
-    while not smart.ready:
-        pass
+    # while not smart.ready:
+    #     pass
 
-    print(smart.ready, smart.patient)
-    body += "<p>Name: {0}</p>".format(smart.human_name(patient.name[0]))
-    body += "<p>Gender: {0}</p>".format(patient.gender)
+    # print(smart.ready, smart.patient)
+    # body += "<p>Name: {0}</p>".format(smart.human_name(patient.name[0]))
+    # body += "<p>Gender: {0}</p>".format(patient.gender)
 
-    # search = pat.Patient.where(struct={'gender': 'male'})
-    # procedures = search.perform_resources(smart.server)
-    # for procedure in procedures:
-    #     your_json = procedure.as_json()
-    #     print(json.dumps(your_json, indent=2, sort_keys=True))
-    #     body += "<p>{0}</p>".format(json.dumps(your_json, indent=2, sort_keys=True))
+    search = pat.Patient.where(struct={'gender': 'male'})
+    procedures = search.perform_resources(smart.server)
+    for procedure in procedures:
+        your_json = procedure.as_json()
+        #body += "<p>{0}</p>".format(json.dumps(your_json, indent=2, sort_keys=True))
+        body += "<p>Patient: {0} {1}</p>".format(your_json['name'][0]['given'][0], your_json['name'][0]['family'])
     return body
 
 
